0eec8e3ad2133177b1a9175ef5a248784d1df2dd
BATMAN292 <89033091+BATMAN292@users.noreply.github.com>
2021-08-25 21:39:04 -0500

    Update funciones.h

--- Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -3,24 +3,26 @@
 #include <iostream>
 using namespace std;
 
+//crear la clase Funciones
 class Funciones{
   private:
-    int x;
+    int x; //variable que recibirán las funciones
 
   public:
+  //declarar funciones
     int sumaIterativa(int);
     int sumaRecursiva(int);
     int sumaDirecta(int);
 };
 
 int Funciones::sumaIterativa(int x){
-  int suma = 0;
+  int suma = 0; //inicio de la variable que juntará los valores que se suman
   if (x==0){
-    return 0;
+    return 0; //si el valor de x es igual a 0, no se realizarán sumas y sólo aparecerá el 0 que indicamos aquí
   };
   for (int i=1; i<=x; i++)
     suma = suma + i;
-  return suma;
+  return suma; //recibimos el valor de suma
 };
 
 int Funciones::sumaRecursiva(int x){
@@ -28,7 +30,7 @@ int Funciones::sumaRecursiva(int x){
     return 0;
   }
   else{
-    return x+sumaRecursiva(x-1);
+    return x+sumaRecursiva(x-1); // "la suma recursiva es como la escena de sueño en Inception" -el profe. Llamamos a la suma a repetirse consigo misma (pero con límite)
   };
 };
 
@@ -37,6 +39,6 @@ int Funciones::sumaDirecta(int x){
     return 0;
   }
   else{
-    return (x*(x+1))/2;
+    return (x*(x+1))/2; //usamos sumas de Gauss para recibir nuestro resultado
   };
 };
